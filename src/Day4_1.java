b4058c11be4b22ed0b0666c3a4b61d1abae7d564
patrickwilliams <piw7176@gmail.com>
2022-12-04 17:42:57 -0500

    start of day 4

---
+++ src/Day4_1.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Day4_1 {
+    public static <Int> void main(String[] args)
+            throws FileNotFoundException {
+
+        // Read in file, split it by new line to get each pair
+        File file = new File("src/Day4_1 input");
+        Scanner scan = new Scanner(file);
+        scan.useDelimiter("\n");
+
+        // Initialize output variable
+        int hits = 0;
+
+        for (int i = 0; i < 6; i++) {
+
+        }
+    }
+}
