b4ac717d9051cbb5354b0f27bde02ee7ee667f86
patrickwilliams <piw7176@gmail.com>
2022-12-04 20:25:04 -0500

    added day 4

--- src/Day4_2.java
+++ src/Day4_2.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Day4_1 {
+public class Day4_2 {
     public static void main(String[] args)
             throws FileNotFoundException {
 
@@ -13,12 +13,11 @@ public class Day4_1 {
         // Initialize output variable
         int hits = 0;
 
+        // if 1 is less than two or three is less than zero == false
         for (int i = 0; i < 1000; i++) {
             String[] pair = scan.next().split("[^0-9]");
-            if (((Integer.parseInt(pair[0]) <= Integer.parseInt(pair[2]))
-                    && (Integer.parseInt(pair[1]) >= Integer.parseInt(pair[3])))
-                    || ((Integer.parseInt(pair[2]) <= Integer.parseInt(pair[0]))
-                    && (Integer.parseInt(pair[3]) >= (Integer.parseInt(pair[1]))))){
+            if (!((Integer.parseInt(pair[1]) < Integer.parseInt(pair[2]))
+                    || (Integer.parseInt(pair[3]) < Integer.parseInt(pair[0])))){
                 hits ++;
                 System.out.println(hits);
             }
